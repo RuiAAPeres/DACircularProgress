e009c8d297b446f495b3d9c0d6a4aa6e89b74981
Rui Peres <rui.peres@mailonline.co.uk>
2015-09-22 11:37:48 +0100

    Carthage enabled

---
+++ DACircularProgress/DACircularProgress/DACircularProgress.h
@@ -0,0 +1,20 @@
+//
+//  DACircularProgress.h
+//  DACircularProgress
+//
+//  Created by Rui Peres on 22/09/2015.
+//  Copyright © 2015 DACircularProgress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DACircularProgress.
+FOUNDATION_EXPORT double DACircularProgressVersionNumber;
+
+//! Project version string for DACircularProgress.
+FOUNDATION_EXPORT const unsigned char DACircularProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DACircularProgress/PublicHeader.h>
+
+#import <DACircularProgress/DACircularProgressView.h>
+#import <DACircularProgress/DALabeledCircularProgressView.h>
